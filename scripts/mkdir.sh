29b7c0d19b1d2c9d7df0eba4eaf7c56b69c19d2a
zyc <yichuanz@amazon.com>
2021-09-08 17:54:54 +0800

    change 090807

--- scripts/mkdir.sh
+++ scripts/mkdir.sh
@@ -3,5 +3,5 @@ curl -O https://bootstrap.pypa.io/get-pip.py
 #python2.7 get-pip.py --user
 #python2.7 -m pip install flask
 #python3.7 get-pip.py --user
-#python3.7 -m pip install Flask
+python3.7 -m pip install Flask
 #pip3 install Flask --user
