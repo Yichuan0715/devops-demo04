62d41bc226e865988057ae2f7d670e14f21840ba
zyc <yichuanz@amazon.com>
2021-09-08 10:37:55 +0800

    change 090803

--- web.py
+++ web.py
@@ -18,7 +18,7 @@ myapp = Flask(__name__)
 @myapp.route('/shutdown', methods=['POST'])
 def shutdown():
     shutdown_server()
-    return 'Server shutting down111...'
+    return 'Server shutting down222...'
 
 
 @myapp.route("/")
